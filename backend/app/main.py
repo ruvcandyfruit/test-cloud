84f1fced116e9cf8df213eee53e8d4cecccaa1b4
Aungaing <mintita.kul@gmail.com>
2026-03-08 09:37:56 +0700

    Connect API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello Cloud Project"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # อนุญาตทุก origin (สำหรับ dev)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/welcome")
+def welcome(name: str):
+    return {"message": f"Welcome to Course Management {name}"}
